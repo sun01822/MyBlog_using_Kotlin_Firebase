a8ddc09b3e85753586ef8ad1526f61419ad1e4f3
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-05-28 21:37:51 +0600

    Blog showed successfully

--- app/src/main/java/com/example/authentication/HomeFragment.kt
+++ app/src/main/java/com/example/authentication/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.example.authentication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.authentication.databinding.FragmentHomeBinding
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 
 
@@ -20,12 +23,15 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater)
         refreshApp()
-
+        showAllBlog()
         return binding.root
     }
+    @SuppressLint("NotifyDataSetChanged")
     private fun refreshApp() {
         val swipeToRefresh = binding.swipeToRefresh
         swipeToRefresh.setOnRefreshListener {
+            showAllBlog()
+            binding.blogRecyclerView.adapter?.notifyDataSetChanged()
             swipeToRefresh.isRefreshing = false
         }
 
@@ -33,7 +39,30 @@ class HomeFragment : Fragment() {
 
     private fun showAllBlog(){
         dataList = arrayListOf()
+        db.collection("Post").get().addOnSuccessListener {
+            if(it.isEmpty){
 
+            }
+            else{
+                it.documents.forEach{singleData->
+                    val model = singleData.toObject(PostModel::class.java)
+                    if(model != null){
+                        dataList.add(model)
+                    }
+                }
+                initRecyclerView()
+            }
+        }.addOnFailureListener {
+
+        }
+
+    }
+
+    private fun initRecyclerView(){
+        binding.blogRecyclerView.hasFixedSize()
+        binding.blogRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        dataList.shuffle()
+        binding.blogRecyclerView.adapter = BlogAdapter(dataList)
     }
 
 }
--- app/src/main/java/com/example/authentication/PostModel.kt
+++ app/src/main/java/com/example/authentication/PostModel.kt
@@ -4,9 +4,9 @@ data class PostModel(
     val profileImage : String?=null,
     val profileName : String?=null,
     val postTime : String?=null,
-    val profileDescription : String?=null,
+    val postDescription : String?=null,
     val postImage : String?=null,
     val postLikes : Int?=null,
     val postLoves : Int?=null,
-    val postUnlike : Int?=null,
+    val postUnlikes : Int?=null,
 )
